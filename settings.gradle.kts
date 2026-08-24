8a84205364cc357a77a2d444201aea6aafb94423
Pedro Neves <phf.neves96@gmail.com>
2026-08-23 23:57:47 -0300

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+git pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "multiplatform-library-template"
+include(":library")
